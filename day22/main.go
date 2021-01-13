16308e4011b9377d85a94c06cdf35eb65bcac3d4
Dan Liberatori <daniel.liberatori@gmail.com>
2021-01-14 08:48:49 +0900

    Solve Day 22 part 1

--- day22/main.go
+++ day22/main.go
@@ -3,8 +3,91 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type card struct {
+	val  int
+	next *card
+	prev *card
+}
+
+// deck contains an ordered list of cards. Because we play cards from the top downard,
+// the .next pointers point toward the bottom of the deck.
+// head                       tail
+//    <--prev        next -->
+type deck struct {
+	head  *card
+	tail  *card
+	count int
+}
+
+func (d *deck) addToBottom(c *card) {
+	c.next = nil
+	c.prev = d.tail
+	if d.tail != nil {
+		d.tail.next = c
+	} else {
+		d.head = c
+	}
+	d.tail = c
+	d.count++
+}
+
+func (d *deck) popHead() *card {
+	// empty deck
+	if d.head == nil {
+		return d.head
+	}
+
+	head := d.head
+
+	// second card in the deck moves up to top spot
+	if head.next != nil {
+		head.next.prev = nil
+	}
+	d.head = head.next
+
+	// popped node does not belong in a deck anymore
+	head.next = nil
+	head.prev = nil
+
+	// update count
+	d.count--
+
+	// empty deck
+	if d.count == 0 {
+		d.tail = nil
+	}
+
+	return head
+}
+
+func (d *deck) score() int {
+	if d.tail == nil {
+		return 0
+	}
+	total := 0
+	i := 1
+	node := d.tail
+	for node != nil {
+		total += node.val * i
+		i++
+		node = node.prev
+	}
+	return total
+}
+
+func (d *deck) toIntSlice() []int {
+	nums := make([]int, 0)
+	node := d.head
+	for node != nil {
+		nums = append(nums, node.val)
+		node = node.next
+	}
+	return nums
+}
 
 func readInput(fname string) string {
 	data, err := ioutil.ReadFile(fname)
@@ -15,8 +98,60 @@ func readInput(fname string) string {
 	return strings.TrimRight(s, "\n")
 }
 
+// parseInput into Player 1's deck and Player 2's deck
+func parseInput(txt string) (deck, deck) {
+	p1 := deck{}
+	p2 := deck{}
+
+	lines := strings.Split(txt, "\n")
+	i := 0
+	for ; lines[i] != ""; i++ {
+		num, err := strconv.Atoi(lines[i])
+		if err != nil {
+			// empty line or "Player 1:"
+			continue
+		}
+		c := card{val: num}
+		p1.addToBottom(&c)
+	}
+	for ; i < len(lines); i++ {
+		num, err := strconv.Atoi(lines[i])
+		if err != nil {
+			// empty line or "Player 2:"
+			continue
+		}
+		c := card{val: num}
+		p2.addToBottom(&c)
+	}
+
+	return p1, p2
+}
+
+func playRound(p1, p2 *deck) int {
+	p1card := p1.popHead()
+	p2card := p2.popHead()
+
+	if p1card.val > p2card.val {
+		p1.addToBottom(p1card)
+		p1.addToBottom(p2card)
+		return 1
+	}
+	p2.addToBottom(p2card)
+	p2.addToBottom(p1card)
+	return 2
+}
+
 func part1(txt string) int {
-	return 0
+	p1, p2 := parseInput(txt)
+
+	var winner int
+	for p1.count > 0 && p2.count > 0 {
+		winner = playRound(&p1, &p2)
+	}
+	if winner == 1 {
+		return p1.score()
+	}
+	return p2.score()
 }
 
 func part2(txt string) int {
--- day22/main_test.go
+++ day22/main_test.go
@@ -1,1 +1,219 @@
 package main
+
+import "testing"
+
+func TestAddToBottomOrdering(t *testing.T) {
+	c1 := card{val: 1}
+	c2 := card{val: 2}
+	c3 := card{val: 3}
+
+	d := deck{}
+	d.addToBottom(&c1)
+	if c1.next != nil || c1.prev != nil {
+		t.Errorf("c1 should be alone in the deck")
+		return
+	}
+
+	d.addToBottom(&c2)
+	if c1.prev != nil || c1.next != &c2 || c2.prev != &c1 {
+		t.Errorf("Deck should contain [1, 2]")
+		return
+	}
+
+	d.addToBottom(&c3)
+	if c1.prev != nil || c1.next != &c2 || c2.prev != &c1 || c2.next != &c3 || c3.prev != &c2 || c3.next != nil {
+		t.Errorf("Deck should contains [1, 2, 3]")
+		return
+	}
+}
+
+func TestAddToBottomHeadTail(t *testing.T) {
+	c1 := card{val: 1}
+	c2 := card{val: 2}
+	c3 := card{val: 3}
+	d := deck{}
+	d.addToBottom(&c1)
+	if d.head != &c1 {
+		t.Error("List head should be c1")
+		return
+	}
+	if d.tail != &c1 {
+		t.Error("List tail should be c1")
+		return
+	}
+	d.addToBottom(&c2)
+	if d.head != &c1 {
+		t.Error("List head should be c1")
+		return
+	}
+	if d.tail != &c2 {
+		t.Error("List tail should be c2")
+		return
+	}
+	d.addToBottom(&c3)
+	if d.head != &c1 {
+		t.Error("List head should be c1")
+		return
+	}
+	if d.tail != &c3 {
+		t.Error("List tail should be c3")
+		return
+	}
+}
+
+func TestAddToBottomCount(t *testing.T) {
+	c1 := card{val: 1}
+	c2 := card{val: 2}
+	c3 := card{val: 3}
+	d := deck{}
+	if d.count != 0 {
+		t.Errorf("0 != %d", d.count)
+	}
+	d.addToBottom(&c1)
+	if d.count != 1 {
+		t.Errorf("1 != %d", d.count)
+	}
+	d.addToBottom(&c2)
+	if d.count != 2 {
+		t.Errorf("2 != %d", d.count)
+	}
+	d.addToBottom(&c3)
+	if d.count != 3 {
+		t.Errorf("3 != %d", d.count)
+	}
+}
+
+func TestPopHeadEmptyList(t *testing.T) {
+	d := deck{}
+	if d.popHead() != nil {
+		t.Error("Should return nil if the deck is empty.")
+	}
+}
+
+func TestPopHead(t *testing.T) {
+	c1 := card{val: 1}
+	c2 := card{val: 2}
+	c3 := card{val: 3}
+	d := deck{}
+	d.addToBottom(&c1)
+	d.addToBottom(&c2)
+	d.addToBottom(&c3)
+
+	head := d.popHead()
+	if head != &c1 {
+		t.Error("Should have popped c1")
+		return
+	}
+	if d.head != &c2 {
+		t.Error("c2 should be on top of the deck after removing c1")
+	}
+	if d.tail != &c3 {
+		t.Errorf("c3 should be on bottom")
+	}
+	if head.prev != nil || head.next != nil {
+		t.Error("c1 should no longer have pointers to any other cards")
+	}
+}
+
+func TestPopHeadCount(t *testing.T) {
+	c1 := card{val: 1}
+	c2 := card{val: 2}
+	c3 := card{val: 3}
+	d := deck{}
+	d.addToBottom(&c1)
+	d.addToBottom(&c2)
+	d.addToBottom(&c3)
+	if d.count != 3 {
+		t.Errorf("3 != %d", d.count)
+		return
+	}
+
+	d.popHead()
+	if d.count != 2 {
+		t.Errorf("2 != %d", d.count)
+		return
+	}
+	d.popHead()
+	if d.count != 1 {
+		t.Errorf("1 != %d", d.count)
+		return
+	}
+	d.popHead()
+	if d.count != 0 {
+		t.Errorf("0 != %d", d.count)
+		return
+	}
+	if d.head != nil {
+		t.Error("Deck should not have a head")
+		return
+	}
+	if d.tail != nil {
+		t.Error("Deck should not have a tail")
+	}
+}
+
+func intSliceEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestParseInput(t *testing.T) {
+	txt := "Player 1:\n9\n2\n6\n3\n1\n\nPlayer 2:\n5\n8\n4\n7\n10"
+	p1, p2 := parseInput(txt)
+	expectedP1 := []int{9, 2, 6, 3, 1}
+	expectedP2 := []int{5, 8, 4, 7, 10}
+	p1cards := p1.toIntSlice()
+	p2cards := p2.toIntSlice()
+	if !intSliceEqual(expectedP1, p1cards) {
+		t.Errorf("%v != %v", expectedP1, p1cards)
+		return
+	}
+	if !intSliceEqual(expectedP2, p2cards) {
+		t.Errorf("%v != %v", expectedP2, p2cards)
+	}
+}
+
+func TestPlayRound01(t *testing.T) {
+	txt := "Player 1:\n9\n2\n6\n3\n1\n\nPlayer 2:\n5\n8\n4\n7\n10"
+	p1, p2 := parseInput(txt)
+	expectedP1 := []int{2, 6, 3, 1, 9, 5}
+	expectedP2 := []int{8, 4, 7, 10}
+
+	roundWinner := playRound(&p1, &p2)
+	if roundWinner != 1 {
+		t.Errorf("Player 1 should have won the round")
+	}
+
+	p1cards := p1.toIntSlice()
+	p2cards := p2.toIntSlice()
+	if !intSliceEqual(expectedP1, p1cards) {
+		t.Errorf("%v != %v", expectedP1, p1cards)
+		return
+	}
+	if !intSliceEqual(expectedP2, p2cards) {
+		t.Errorf("%v != %v", expectedP2, p2cards)
+	}
+}
+
+func TestScore(t *testing.T) {
+	nums := []int{3, 2, 10, 6, 8, 5, 9, 4, 7, 1}
+	cards := make([]card, len(nums))
+	for i, num := range nums {
+		cards[i] = card{val: num}
+	}
+	d := deck{}
+	for i := range cards {
+		d.addToBottom(&(cards[i]))
+	}
+	score := d.score()
+	if score != 306 {
+		t.Errorf("306 != %d", score)
+	}
+}
