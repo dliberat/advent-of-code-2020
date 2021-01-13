4968fdcd75e688fd212cff02ebe4a8fe5e9a4012
Dan Liberatori <daniel.liberatori@gmail.com>
2021-01-14 07:20:53 +0900

    Solve day 24 part 1

--- day24/main.go
+++ day24/main.go
@@ -1,3 +1,28 @@
+/*
+Every tile has six neighbors: east, southeast, southwest, west, northwest, and northeast.
+These directions are given in your list, respectively, as e, se, sw, w, nw, and ne.
+
+      /\ /\
+     |  |  |
+     /\ /\ /\
+    |  |  |  |  --> x
+	 \/ \/ \/
+	  |  |  |
+	   \/ \/
+			 \
+			  \
+			   y
+
+Tiles can be addressed using a 2-dimensional coordinate system.
+With the axes as defined in the image above, the directions in the puzzle input
+correspond to the following tiles relative to the center tile:
+e  = x+1, y
+w  = x-1, y
+se = x, y+1
+nw = x, y-1
+sw = x-1, y+1
+ne = x+1, y-1
+*/
 package main
 
 import (
@@ -5,6 +30,12 @@ import (
 	"io/ioutil"
 	"strings"
 )
+
+type coord struct {
+	x int
+	y int
+	z int
+}
 
 func readInput(fname string) string {
 	data, err := ioutil.ReadFile(fname)
@@ -15,8 +46,51 @@ func readInput(fname string) string {
 	return strings.TrimRight(s, "\n")
 }
 
+// move from the starting tile to a new tile using the directions given in string s.
+// s is a sequence of directions 'e', 'w', 'se', 'sw', 'ne', or 'nw' with no delimiters.
+func move(s string, start coord) coord {
+	end := start
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'e' {
+			end.x++
+		} else if s[i] == 'w' {
+			end.x--
+		} else if s[i] == 's' {
+			if s[i+1] == 'e' {
+				end.y++
+			} else {
+				end.x--
+				end.y++
+			}
+			i++
+		} else if s[i] == 'n' {
+			if s[i+1] == 'e' {
+				end.y--
+				end.x++
+			} else {
+				end.y--
+			}
+			i++
+		}
+	}
+	return end
+}
+
 func part1(txt string) int {
-	return 0
+	tileset := make(map[coord]bool, 0) // false = white, true = black
+	for _, line := range strings.Split(txt, "\n") {
+		tile := move(line, coord{0, 0, 0})
+		tileset[tile] = !tileset[tile]
+	}
+
+	// count black tiles
+	total := 0
+	for _, v := range tileset {
+		if v {
+			total++
+		}
+	}
+	return total
 }
 
 func part2(txt string) int {
--- day24/main_test.go
+++ day24/main_test.go
@@ -1,1 +1,47 @@
 package main
+
+import "testing"
+
+func TestMove01(t *testing.T) {
+	txt := "esew"
+	tile := move(txt, coord{0, 0, 0})
+	if tile.x != 0 || tile.y != 1 || tile.z != 0 {
+		t.Errorf("Expected (0, 1, 0), but got (%v)", tile)
+	}
+}
+
+func TestMove02(t *testing.T) {
+	txt := "nwwswee"
+	tile := move(txt, coord{0, 0, 0})
+	if tile.x != 0 || tile.y != 0 || tile.z != 0 {
+		t.Errorf("Expected (0, 0, 0), but got (%v)", tile)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	txt := `sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew`
+
+	res := part1(txt)
+	if res != 10 {
+		t.Errorf("Expected 10 black tiles but got %d", res)
+	}
+}
