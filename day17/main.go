5d43c4510208dc93aea7aeac974cb73fdd7a72a4
Dan Liberatori <daniel.liberatori@gmail.com>
2021-01-13 21:53:32 +0900

    Solve day 17

--- day17/main.go
+++ day17/main.go
@@ -1,3 +1,18 @@
+/*
+Conway's Game of Life in 3D with an infinite grid.
+
+A cell is represented using six bits
+
++----------------- bits 6-7: unused
+|   +------------- bits 1-5: neighbor count
+|   |         +--- bit 0: cell state (1: alive, 0: dead)
+|   |         |
+0 0 0 0 0 0 0 0
+
+In this representation, an off cell with no active neighbors is 0.
+Since cells with no neighbors do not change and do not affect other cells,
+they do not need to be stored in the hashmap.
+*/
 package main
 
 import (
@@ -5,6 +20,107 @@ import (
 	"io/ioutil"
 	"strings"
 )
+
+type coord struct {
+	x int
+	y int
+	z int
+	w int
+}
+
+func (c *coord) toString() string {
+	return fmt.Sprintf("(%d, %d, %d, %d)", c.x, c.y, c.z, c.w)
+}
+
+type cellmap struct {
+	cells map[coord]int
+	is4d  bool // switch between 3D and 4D for parts 1 and 2 of the question
+}
+
+// setCell turns a cell on (switch from dead to alive).
+// Note: The cell MUST have been dead or else neighbor counts will be incorrect
+func (cm *cellmap) setCell(x, y, z, w int) {
+	minW, maxW := 0, 0
+	if cm.is4d {
+		minW, maxW = -1, 1
+	}
+	for ow := minW; ow <= maxW; ow++ {
+		for ox := -1; ox <= 1; ox++ {
+			for oy := -1; oy <= 1; oy++ {
+				for oz := -1; oz <= 1; oz++ {
+					loc := coord{x + ox, y + oy, z + oz, w + ow}
+
+					if ox == 0 && oy == 0 && oz == 0 && ow == 0 {
+						cm.cells[loc] |= 1 // turn cell on
+					} else {
+						cm.cells[loc] += 2 // add 1 to neighbor count
+					}
+				}
+			}
+		}
+	}
+}
+
+// clearCell turns a cell off (switch from alive to dead).
+// Note: The cell MUST have been alive or else neighbor counts will be incorrect
+func (cm *cellmap) clearCell(x, y, z, w int) {
+	minW, maxW := 0, 0
+	if cm.is4d {
+		minW, maxW = -1, 1
+	}
+	for ow := minW; ow <= maxW; ow++ {
+		for ox := -1; ox <= 1; ox++ {
+			for oy := -1; oy <= 1; oy++ {
+				for oz := -1; oz <= 1; oz++ {
+					loc := coord{x + ox, y + oy, z + oz, w + ow}
+
+					if ox == 0 && oy == 0 && oz == 0 && ow == 0 {
+						cm.cells[loc]-- // turn cell off
+					} else {
+						cm.cells[loc] -= 2 // subtract 1 from neighbor count
+					}
+
+					if cm.cells[loc] == 0 {
+						delete(cm.cells, loc)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (cm *cellmap) getNextIter() cellmap {
+	newCm := makeCellMap(cm.is4d)
+
+	for loc, cell := range cm.cells {
+		neighborCount := cell >> 1
+		if cell%2 == 1 {
+			// cell is active
+			if neighborCount == 2 || neighborCount == 3 {
+				newCm.setCell(loc.x, loc.y, loc.z, loc.w)
+			}
+		} else {
+			// cell is inactive
+			if neighborCount == 3 {
+				newCm.setCell(loc.x, loc.y, loc.z, loc.w)
+			}
+		}
+	}
+
+	return newCm
+}
+
+func (cm *cellmap) countActive() int {
+	total := 0
+	for _, cell := range cm.cells {
+		total += cell % 2
+	}
+	return total
+}
+
+func makeCellMap(is4d bool) cellmap {
+	return cellmap{cells: make(map[coord]int, 0), is4d: is4d}
+}
 
 func readInput(fname string) string {
 	data, err := ioutil.ReadFile(fname)
@@ -15,12 +131,47 @@ func readInput(fname string) string {
 	return strings.TrimRight(s, "\n")
 }
 
+func input2cellmap(txt string, is4d bool) cellmap {
+	cm := makeCellMap(is4d)
+	split := strings.Split(txt, "\n")
+	rowoffset := len(split) / 2
+	coloffset := len(split[0]) / 2
+	for i, row := range split {
+		for j, char := range row {
+			if char == '#' {
+				y := i - rowoffset
+				x := j - coloffset
+				cm.setCell(x, y, 0, 0)
+			}
+		}
+	}
+	return cm
+}
+
 func part1(txt string) int {
-	return 0
+	cm := input2cellmap(txt, false)
+
+	// boot sequence = six iterations
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	return cm.countActive()
 }
 
 func part2(txt string) int {
-	return 0
+	cm := input2cellmap(txt, true)
+
+	// boot sequence = six iterations
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	return cm.countActive()
 }
 
 func main() {
--- day17/main_test.go
+++ day17/main_test.go
@@ -1,1 +1,229 @@
 package main
+
+import "testing"
+
+func TestSetCellNoNeighbors(t *testing.T) {
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, 0, 0)
+
+	if len(cm.cells) != 3*3*3 {
+		t.Error("One cell should be surrounded in a 3x3x3 grid.")
+	}
+
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				loc := coord{x, y, z, 0}
+
+				if x == 0 && y == 0 && z == 0 {
+					if cm.cells[loc] != 1 {
+						t.Errorf("Cell %s should be on with no neighbors.", loc.toString())
+					}
+				} else {
+					if (cm.cells[loc] >> 1) != 1 {
+						t.Errorf("Cell %s should have a single neighbor at (0,0,0)", loc.toString())
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestSetCellX(t *testing.T) {
+	/*
+			z = 0
+			        x
+			    -1 0  1  2
+			-1	-  -  -  -
+		 y   0	-  X  X  -
+			 1  -  -  -  -
+	*/
+
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, 0, 0)
+	cm.setCell(1, 0, 0, 0)
+
+	if len(cm.cells) != 4*3*3 {
+		t.Error("Two cells should be surrounded in a 4x3x3 grid.")
+	}
+
+	for x := -1; x <= 2; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				loc := coord{x: x, y: y, z: z}
+
+				if (x == 0 && y == 0 && z == 0) || (x == 1 && y == 0 && z == 0) {
+					if cm.cells[loc] != 3 {
+						t.Errorf("Cell %s should be on 1 neighbor.", loc.toString())
+					}
+				} else if x == -1 || x == 2 {
+					if (cm.cells[loc] >> 1) != 1 {
+						t.Errorf("Cell %s should have a single neighbor", loc.toString())
+					}
+				} else {
+					if (cm.cells[loc] >> 1) != 2 {
+						t.Errorf("Cell %s should have two neighbors", loc.toString())
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestSetCellSpacing(t *testing.T) {
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, -5, 0)
+	cm.setCell(0, 0, 5, 0)
+	// empty cells between the populated regions should not be in map
+	if len(cm.cells) != 2*(3*3*3) {
+		t.Errorf("Expected 2*3*3*3 cells but got %d", len(cm.cells))
+	}
+}
+
+func TestClearCellEmpty(t *testing.T) {
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, 0, 0)
+	cm.clearCell(0, 0, 0, 0)
+	if len(cm.cells) != 0 {
+		t.Errorf("cellmap should be empty but found %d cells", len(cm.cells))
+	}
+}
+
+func TestClearCellY(t *testing.T) {
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, 0, 0)
+	cm.setCell(0, -1, 0, 0)
+
+	if len(cm.cells) != 3*4*3 {
+		t.Error("One cell should be surrounded in a 3x4x3 grid.")
+	}
+
+	cm.clearCell(0, -1, 0, 0)
+
+	if len(cm.cells) != 3*3*3 {
+		t.Error("One cell should be surrounded in a 3x3x3 grid.")
+	}
+
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				loc := coord{x: x, y: y, z: z}
+
+				if x == 0 && y == 0 && z == 0 {
+					if cm.cells[loc] != 1 {
+						t.Errorf("Cell %s should be on with no neighbors.", loc.toString())
+					}
+				} else {
+					if (cm.cells[loc] >> 1) != 1 {
+						t.Errorf("Cell %s should have a single neighbor at (0,0,0)", loc.toString())
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestClearCellSpacing(t *testing.T) {
+	/*
+		z = 0
+			- - - -
+			- X X -
+			- - - -
+		z = 1
+			- - - -
+			- X X -
+			- - - -
+		z = 2
+			- - - -
+			- - - -
+			- - - -
+		z = 3
+			- - - -
+			- X X -
+			- - - -
+
+		Every node in the range -1 <= x <= 2; -1 <= y <= 1; -1 <= z <= 4 has neighbors.
+		However, if we delete the cells at (0,0,3), (0,0,1), (1,0,1) and (1,0,0),
+		then the two remaining cells will be isolated.
+	*/
+	cm := makeCellMap(false)
+	cm.setCell(0, 0, 0, 0)
+	cm.setCell(1, 0, 0, 0)
+	cm.setCell(0, 0, 1, 0)
+	cm.setCell(1, 0, 1, 0)
+	cm.setCell(1, 0, 3, 0)
+	cm.setCell(0, 0, 3, 0)
+
+	if len(cm.cells) != 4*3*6 {
+		t.Errorf("Expected 4*3*6 cells but got %d", len(cm.cells))
+	}
+
+	cm.clearCell(0, 0, 1, 0)
+	cm.clearCell(0, 0, 3, 0)
+	cm.clearCell(1, 0, 1, 0)
+	cm.clearCell(1, 0, 0, 0)
+	if len(cm.cells) != 2*(3*3*3) {
+		t.Errorf("Expected two isolated 3x3x3 regions of cells but got %d", len(cm.cells))
+	}
+}
+
+func TestInput2CellMap(t *testing.T) {
+	txt := "...\n.#.\n..."
+	cm := input2cellmap(txt, false)
+
+	if len(cm.cells) != 3*3*3 {
+		t.Error("One cell should be surrounded in a 3x3x3 grid.")
+	}
+
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				loc := coord{x: x, y: y, z: z}
+
+				if x == 0 && y == 0 && z == 0 {
+					if cm.cells[loc] != 1 {
+						t.Errorf("Cell %s should be on with no neighbors.", loc.toString())
+					}
+				} else {
+					if (cm.cells[loc] >> 1) != 1 {
+						t.Errorf("Cell %s should have a single neighbor at (0,0,0)", loc.toString())
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestCountAfterSixIters(t *testing.T) {
+	txt := ".#.\n..#\n###"
+	cm := input2cellmap(txt, false)
+
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+
+	total := cm.countActive()
+	if total != 112 {
+		t.Errorf("Expected 112 active cubes but counted %d", total)
+	}
+}
+
+func TestCountAfterSixIters4d(t *testing.T) {
+	txt := ".#.\n..#\n###"
+	cm := input2cellmap(txt, true)
+
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+	cm = cm.getNextIter()
+
+	total := cm.countActive()
+	if total != 848 {
+		t.Errorf("Expected 848 active cubes but counted %d", total)
+	}
+}
