6f542238592d44aa0a9aa9821b1c1a8f52e37b0e
Dan Liberatori <daniel.liberatori@gmail.com>
2021-01-17 11:59:02 +0900

    Solve day 23 part 1

--- day23/main.go
+++ day23/main.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+type node struct {
+	val  int
+	next *node
+}
+
 func readInput(fname string) string {
 	data, err := ioutil.ReadFile(fname)
 	if err != nil {
@@ -15,8 +20,117 @@ func readInput(fname string) string {
 	return strings.TrimRight(s, "\n")
 }
 
-func part1(txt string) int {
-	return 0
+func parseInput(txt string) []node {
+	nums := make([]node, len(txt))
+	for i := 0; i < len(txt); i++ {
+		val := int(txt[i]) - 48
+		nums[i] = node{val: val}
+	}
+	for i := 0; i < len(nums); i++ {
+		next := (i + 1) % len(nums)
+		nums[i].next = &nums[next]
+	}
+	return nums
+}
+
+func findTail(head *node) *node {
+	prev := head
+	cur := head.next
+	for cur.val != head.val {
+		prev = cur
+		cur = cur.next
+	}
+	return prev
+}
+
+// removeN nodes from the linked list starting with the node
+// immediately following head. Returns a pointer
+// to the first removed node, which is the head of a new list
+func removeN(head *node, n int) *node {
+	ret := head.next
+
+	cur := head
+	for i := 0; i < n; i++ {
+		cur = cur.next
+	}
+	// cur is now the last node we need to extract
+	head.next = cur.next
+	cur.next = ret
+
+	return ret
+}
+
+// insert node x immediately after the node with the value dest
+// Returns true if the node was successfully inserted.
+// Returns false if the value dest could not be found
+func insert(head, x *node, dest int) bool {
+	headVal := head.val
+	cur := head
+	for cur.val != dest {
+		cur = cur.next
+		if cur.val == headVal {
+			return false
+		}
+	}
+	xtail := findTail(x)
+	xtail.next = cur.next
+	cur.next = x
+	return true
+}
+
+func move(head *node, nCups int) *node {
+	// Pick up 3 cups that are immediately clockwise of current cup
+	pickup := removeN(head, 3)
+
+	// Select destination cup (label equal to current cup minus 1)
+	destinationCupVal := head.val - 1
+	if destinationCupVal < 1 {
+		destinationCupVal += nCups
+	}
+
+	// If this would select one of the cups that was just picked up,
+	// keep subtracting 1 until it finds a cup that wasn't picked up.
+	// Place the picked up cups immediately clockwise of the current cup
+	for !insert(head, pickup, destinationCupVal) {
+		destinationCupVal--
+		if destinationCupVal < 1 {
+			destinationCupVal += nCups
+		}
+	}
+
+	// New current cup is the cup immediately clockwise of the current cup
+	return head.next
+}
+
+func collectCups(head *node) []int {
+	for head.val != 1 {
+		head = head.next
+	}
+	head = head.next
+	nums := []int{1}
+	for head.val != 1 {
+		nums = append(nums, head.val)
+		head = head.next
+	}
+	return nums
+}
+
+func part1(txt string) string {
+	numCups := len(txt)
+	cups := parseInput(txt)
+	head := &cups[0]
+
+	for i := 0; i < 100; i++ {
+		head = move(head, numCups)
+	}
+
+	collected := collectCups(head)
+	collected = collected[1:] // cup #1 is not needed for the output
+	s := ""
+	for _, num := range collected {
+		s = fmt.Sprintf("%s%d", s, num)
+	}
+	return s
 }
 
 func part2(txt string) int {
--- day23/main_test.go
+++ day23/main_test.go
@@ -1,1 +1,156 @@
 package main
+
+import "testing"
+
+func TestParseInput(t *testing.T) {
+	nodes := parseInput("186524973")
+	expected := []int{1, 8, 6, 5, 2, 4, 9, 7, 3}
+	head := &nodes[0]
+	if head.val != expected[0] {
+		t.Errorf("%d != %d", expected[0], head.val)
+		return
+	}
+	expected = expected[1:]
+
+	current := head.next
+	for current != head {
+		if current.val != expected[0] {
+			t.Errorf("%d != %d", expected[0], current.val)
+			return
+		}
+		current = current.next
+		expected = expected[1:]
+	}
+}
+
+func TestRemoveN(t *testing.T) {
+	nodes := parseInput("186524973")
+	// remove nodes 8,6,5
+	one := &nodes[0]
+	eight := removeN(one, 3)
+
+	two := one.next
+	four := two.next
+	nine := four.next
+	seven := nine.next
+	three := seven.next
+	looparound := three.next
+	if one.val != 1 || two.val != 2 || four.val != 4 || nine.val != 9 || seven.val != 7 || three.val != 3 || looparound != one {
+		t.Errorf("Incorrect node removal: [%d, %d, %d, %d, %d, %d]", one.val, two.val, four.val, nine.val, seven.val, three.val)
+		return
+	}
+
+	six := eight.next
+	five := six.next
+	looparound = five.next
+	if eight.val != 8 || six.val != 6 || five.val != 5 || looparound != eight {
+		t.Errorf("Incorrect nodes removed: [%d, %d, %d]", eight.val, six.val, five.val)
+	}
+
+}
+
+func TestFindTail(t *testing.T) {
+	nodes := parseInput("186524973")
+	head := &nodes[0]
+	tail := findTail(head)
+	if tail.val != 3 {
+		t.Errorf("Tail should be 3, got %d", tail.val)
+	}
+}
+
+func TestContains(t *testing.T) {
+	nodes := parseInput("186524973")
+	head := &nodes[0]
+	trues := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	falses := []int{0, -1, 10}
+	for _, n := range trues {
+		if !contains(head, n) {
+			t.Errorf("Linked list should contain %d", n)
+			return
+		}
+	}
+	for _, n := range falses {
+		if contains(head, n) {
+			t.Errorf("Linked list should not contain %d", n)
+			return
+		}
+	}
+}
+
+func TestInsert01(t *testing.T) {
+	nodes := parseInput("13")
+	toInsert := parseInput("2")
+	destHead := &nodes[0]
+	insertHead := &toInsert[0]
+
+	res := insert(destHead, insertHead, 1)
+	if !res {
+		t.Error("Node should have been successfully inserted.")
+		return
+	}
+	if destHead.val != 1 {
+		t.Errorf("List head should be 1")
+		return
+	}
+	two := destHead.next
+	if two.val != 2 {
+		t.Errorf("Second node in list should be 2")
+		return
+	}
+	three := two.next
+	if three.val != 3 {
+		t.Errorf("Third node in list should be 3")
+		return
+	}
+	looparound := three.next
+	if looparound != destHead {
+		t.Errorf("List should loop back around to 1")
+	}
+}
+
+func assertListEqual(head *node, list []int) bool {
+	for len(list) > 0 {
+		if head.val != list[0] {
+			return false
+		}
+		list = list[1:]
+		head = head.next
+	}
+	return true
+}
+
+func TestMove01(t *testing.T) {
+	nodes := parseInput("389125467")
+	three := &nodes[0]
+	expected := []int{3, 2, 8, 9, 1, 5, 4, 6, 7}
+	newCurrentCup := move(three, 9)
+	if !assertListEqual(three, expected) {
+		t.Error("node order does not match expected")
+		return
+	}
+	if newCurrentCup.val != 2 {
+		t.Errorf("New current cup should be 2, but got %d", newCurrentCup.val)
+	}
+}
+
+func TestMove02(t *testing.T) {
+	nodes := parseInput("289154673")
+	two := &nodes[0]
+	expected := []int{2, 5, 4, 6, 7, 8, 9, 1, 3}
+	newCurrentCup := move(two, 9)
+	if !assertListEqual(two, expected) {
+		t.Error("node order does not match expected")
+		return
+	}
+	if newCurrentCup.val != 5 {
+		t.Errorf("New current cup should be 5, but got %d", newCurrentCup.val)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	txt := "389125467"
+	res := part1(txt)
+	if res != "67384529" {
+		t.Errorf("67384529 != res")
+	}
+}
